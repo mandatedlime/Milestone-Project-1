4c21eb2af1fa8459aa97188d0075852c200048b1
Joram Obbus <joramobbus@gmail.com>
2022-04-28 19:50:04 -0700

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//making character appear 
+
+let mainCharacter = document.createElement('img')
+mainCharacter.src = "male/Walk (1).png"
+// mainCharacter.style.position = "relative"
+// mainCharacter.style.left = "450px"
+// mainCharacter.style.bottom = "200px"
+document.body.append(mainCharacter)
