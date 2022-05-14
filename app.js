5a074831707a39cee6e1844483fb5aa7b74714d0
Joram Obbus <joramobbus@gmail.com>
2022-05-13 18:55:58 -0700

    updates

--- app.js
+++ app.js
@@ -1,60 +1,78 @@
-//making character appear 
+import { setupGround, updateGround } from "./ground.js"
 
-// let mainCharacter = document.createElement('img')
-// mainCharacter.src = "male/Walk (1).png"
-// mainCharacter.style.position = "relative"
-// mainCharacter.style.left = "450px"
-// mainCharacter.style.bottom = "200px"
-// document.body.append(mainCharacter)
+// sets world scale
+const WORLD_WIDTH = 100
+const WORLD_HEIGHT = 100
+const SPEED_SCALE_INCREASE = 0.0001
 
-var canvas = document.querySelector("canvas")
-var c = canvas.getContext('2d');
-
-let moveBy = 10;
-window.addEventListener('load', () => {
-    canvas.style.position = 'absolute';
-    canvas.style.left = 0;
-    canvas.style.top = 0;
-});
+// creating the worldElem
+const worldElem = document.querySelector("[data-world]")
+const scoreElem = document.querySelector("[data-score]")
+const startScreenElem = document.querySelector("[data-start-screen]")
 
 
+// funtion to scale the world
+setPixelToWorldScale()
+window.addEventListener("resize", setPixelToWorldScale)
+document.addEventListener("keydown", handleStart,  { once: true})
 
-// window.addEventListener('keyup', (e) => {
-//     switch (e.key) {
-//         case 'ArrowLeft':
-//             canvas.style.left = parseInt(canvas.style.left) - moveBy + 'px';
-//             break;
-//         case 'ArrowRight':
-//             canvas.style.left = parseInt(canvas.style.left) + moveBy + 'px';
-//             break;
-//         case 'ArrowUp':
-//             canvas.style.top = parseInt(canvas.style.top) - moveBy + 'px';
-//             break;
-//         case 'ArrowDown':
-//             canvas.style.top = parseInt(canvas.style.top) + moveBy + 'px';
-//             break;
-//     }
-// });
 
-mainCharacter.width = window,innerWidth;
-mainCharacter.height = window.innerHeight;
-
-// empty array for images 
-
-var images = [];
-images.length = 10;
-
-// push image to the array 
-
-for(var i = 1 ; i < images.length ; i++){
-    images[i]= new Image();
-    images[i].src = 'Walk (' + i.toString() + ').png';
-}
-var i = 1;
-setInterval(function(){
-    i++;
-    if(i >= 10){
-        i = 1;
+// create update loop to update positons on screen
+let lastTime
+let speedScale
+let score
+function update(time) {
+    if (lastTime == null) {
+        lastTime = time
+        window.requestAnimationFrame(update)
+        return
     }
-    c.drawImage(images[i],50,50,50,50)
-},100)
+
+    const delta = time - lastTime
+    
+    updateGround(delta, speedScale)
+    updateSpeedScale(delta)
+    updateScore(delta)
+
+    lastTime = time
+    window.requestAnimationFrame(update)
+}
+
+
+function updateSpeedScale(delta) {
+    speedScale += delta * SPEED_SCALE_INCREASE
+}
+
+function updateScore(delta){
+    score += delta * 0.01
+    scoreElem.textContent = Math.floor(score)
+}
+
+function handleStart() {
+    lastTime = null
+    setupGround()
+    startScreenElem.classList.add("hide")
+    speedScale = 1
+    score = 0
+    window.requestAnimationFrame(update)
+}
+
+
+// setting the screen size
+function setPixelToWorldScale() {
+
+    // if our window is wider than our world then we need to pixel scale based on width , other wise base it on height
+    let worldToPixelScale
+    if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT)
+    {
+       worldToPixelScale = window.innerWidth / WORLD_WIDTH 
+    } else {
+        worldToPixelScale = window.innerHeight / WORLD_HEIGHT
+    }
+
+
+    worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
+    worldElem.style.width = `${WORLD_HEIGHT * worldToPixelScale}px`
+
+}
+
