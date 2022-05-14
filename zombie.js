2e54a2c411b1e0d1efbf5bcc84dfd93c9ba64301
Joram Obbus <joramobbus@gmail.com>
2022-05-13 19:29:31 -0700

    updates

--- zombie.js
+++ zombie.js
@@ -1,9 +1,28 @@
+// setting the variable for the zombie
 const zombieElem = document.querySelector("[data-dino]")
+const JUMP_SPEED = .45
+const GRAVITY = .011
+const ZOMBIE_FRAME_COUNT = 2
+const FRAME_TIME = 100
 
+// sending the function to app.js
+let isJumping
 export function setupZombie() {
-
+    isJumping = false
 }
 
 export function updateZombie(delta, speedScale) {
+    handleRun()
+    handleJump()
+}
+
+function handleRun() {
+    if (isJumping) {
+        zombieElem.src = "imgs/Walk(3).png"
+        return
+    }
+}
+
+function handleJump(){
 
 }
